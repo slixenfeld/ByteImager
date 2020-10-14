891c44a5ba26f7e6d2449ab79c85f4999c9e2355
slxfld <31144712+slxfld@users.noreply.github.com>
2020-10-14 19:24:12 +0200

    Add classes

---
+++ ImageDataEncoder/src/GUI/OutputPreview.java
@@ -0,0 +1,40 @@
+package GUI;
+
+import Utility.Util;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class OutputPreview extends JPanel {
+
+
+    private BufferedImage image = new BufferedImage(Util.imageSize, Util.imageSize, BufferedImage.TYPE_INT_RGB);
+
+    public OutputPreview() {
+        applyDefaultSettings();
+    }
+
+    public void applyDefaultSettings() {
+        this.setSize(image.getWidth(),image.getHeight());
+    }
+
+    public void updateImage(BufferedImage image) {
+        this.image = image;
+        repaint();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(image, 0, 0, this);
+    }
+
+    public BufferedImage getImage() {
+        return image;
+    }
+
+    public void setImage(BufferedImage image) {
+        this.image = image;
+    }
+}
---
+++ ImageDataEncoder/src/Utility/Util.java
@@ -0,0 +1,14 @@
+package Utility;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class Util {
+
+    public static int imageSize = 0xFF;
+
+    public static void log(String line) {
+        Date time = Calendar.getInstance().getTime();
+        System.out.println(time + " " + line);
+    }
+}
