435385cc38e0e32585e12a0e4866b0874a98fae2
slxfld <31144712+slxfld@users.noreply.github.com>
2021-01-09 20:17:57 +0100

    remove warnings

--- src/de/GUI/panes/FileEncodingPane.java
+++ src/de/GUI/panes/FileEncodingPane.java
@@ -2,7 +2,6 @@ package de.GUI.panes;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
 
 import javax.swing.JButton;
 import javax.swing.JScrollPane;
